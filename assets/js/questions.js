ec75fd3493d122b6b6a5fed8628ad04eda45e4e3
TomScott <contact@tascott.co.uk>
2023-01-14 23:10:12 +0000

    Add some real questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,32 @@
 let questions = [
     {
-        question: 'Example 1',
-        options: ['one', 'two', 'three'],
-        answer: 'one'
+        question: 'Which function is used to serialize an object into a JSON string in Javascript?',
+        options: ['stringify()', 'parse()', 'convert()', 'None of the above'],
+        answer: 'stringify()'
         },
     {
-        question: 'Example 2',
-        options: ['four', 'five', 'six'],
-        answer: 'six'
+        question: 'Which of the following are closures in Javascript?',
+        options: ['Variables', 'Functions', 'Objects', 'All of the above'],
+        answer: 'All of the above'
+    },
+    {
+        question: 'Which of the following methods is used to access HTML elements using Javascript?',
+        options: ['getElementById()', 'getElementsByClassName()', 'A & B', 'None of the above'],
+        answer: 'A & B'
+    },
+    {
+        question: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
+        options: ['Throws an error', 'Ignores the statement', 'Gives a warning', 'None of the above'],
+        answer: 'Ignores the statement'
+    },
+    {
+        question: 'How can a datatype be declared to be a constant type?',
+        options: ['const', 'var', 'let', 'constant'],
+        answer: 'const'
+    },
+    {
+        question: 'What keyword is used to check whether a given property is valid or not?',
+        options: ['in', 'is in', 'exists', 'lies'],
+        answer: 'in'
     }
 ]
