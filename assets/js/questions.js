1a8dd06865f8c7b986e2329d505b34e7b3a28004
TomScott <contact@tascott.co.uk>
2023-01-14 18:23:55 +0000

    Make the questions an array of objects rather than object containing objects

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,12 @@
-let questions = {
-    question1: {
+let questions = [
+    {
         question: 'Example 1',
         options: ['one', 'two', 'three'],
         answer: 'one'
-    },
-    question2: {
+        },
+    {
         question: 'Example 2',
         options: ['four', 'five', 'six'],
         answer: 'six'
     }
-}
+]
